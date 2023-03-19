7deb0d2f64ddd0ea612196458fa228f9576e26b4
MohdAnwar03 <114156804+MohdAnwar03@users.noreply.github.com>
2023-03-19 18:38:02 +0530

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 
-const Header = () => {}
+const Header = () => {
+  return(
+    <div id="header-component>
+    <h1>Newton School</h1>
+    </div>
+    )
+}
 
 
 export default Header;
